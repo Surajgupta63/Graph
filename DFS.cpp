82422ece4b0505c7904e98d9dd42e9a31d06996a
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-20 23:52:46 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,21 +1,24 @@
 // DFS Traversal
-    // T.C : O(V + E)
-    // S.C : O(V)
-    void DFS(vector<vector<int>>& adj, int u, vector<bool> &visited, vector<int> &ans){
-        if(visited[u]) return;
-        
-        visited[u] = true;
-        ans.push_back(u);
-        
-        for(auto &v : adj[u]){
-            if(!visited[v]){
-                DFS(adj, v, visited, ans);
+// T.C : O(V + E)
+// S.C : O(V)
+class Solution{
+    public:
+        void DFS(vector<vector<int>>& adj, int u, vector<bool> &visited, vector<int> &ans){
+            if(visited[u]) return;
+            
+            visited[u] = true;
+            ans.push_back(u);
+            
+            for(auto &v : adj[u]){
+                if(!visited[v]){
+                    DFS(adj, v, visited, ans);
+                }
             }
         }
-    }
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
-        vector<int> ans;
-        vector<bool> visited(adj.size(), false);
-        DFS(adj, 0, visited, ans);
-        return ans;
-    }
+        vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+            vector<int> ans;
+            vector<bool> visited(adj.size(), false);
+            DFS(adj, 0, visited, ans);
+            return ans;
+        }
+};
