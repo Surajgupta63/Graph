ff7870d452f4f8d3a3a4b89cf90bf50d4d4f768a
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-21 19:29:24 +0530

    Update Undirected Graph Cycle.cpp

--- Undirected Graph Cycle.cpp
+++ Undirected Graph Cycle.cpp
@@ -29,3 +29,45 @@ class Solution{
         
     }
 };
+
+// Using BFS
+// T.C : O(V+E)
+// S.C : O(V)
+class Solution{
+    public:
+    bool isCycleBFS(vector<vector<int>>& adj, int u, vector<bool> &visited){
+        queue<pair<int, int>> q;
+        q.push({u, -1});
+        visited[u] = true;
+        
+        while(!q.empty()){
+            pair<int, int> p = q.front();
+            q.pop();
+            
+            int node   = p.first;
+            int parent = p.second;
+            
+            for(int &v : adj[node]){
+                if(!visited[v]){
+                    q.push({v, node});
+                    visited[v] = true;
+                }
+                else if(v != parent) return true;
+            }
+        }
+        return false;    
+    }
+    bool isCycle(vector<vector<int>>& adj) {
+        
+        int V = adj.size();
+        vector<bool> visited(V, false);
+        
+        for(int i=0; i<V; i++){
+            if(!visited[i] && isCycleBFS(adj, i, visited)){
+                return true;
+            }
+        }
+        return false;   
+    }
+};
+
