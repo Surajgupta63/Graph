2ede4d044ec1d86d5579464840a2044958b56bbd
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-21 20:10:37 +0530

    Create Cycle in a Directed Graph.cpp

---
+++ Cycle in a Directed Graph.cpp
@@ -0,0 +1,27 @@
+
+class Solution{
+    public:
+    bool isCycleDFS(vector<vector<int>> &adj, int u, vector<bool> &visited, vector<bool> &currRec){
+        visited[u] = true;
+        currRec[u] = true;
+        
+        for(int &v : adj[u]){
+            if(visited[v] && currRec[v]) return true;
+            
+            if(isCycleDFS(adj, v, visited, currRec)) return true;
+        }
+        
+        currRec[u] = false;
+        
+        return false;
+    }
+    bool isCyclic(int V, vector<vector<int>> adj) {
+        vector<bool> visited(V, false);
+        vector<bool> currRec(V, false);
+        
+        for(int i=0; i<V; i++){
+            if(!visited[i] && isCycleDFS(adj, i, visited, currRec)) return true;
+        }
+        return false;
+    }
+};
