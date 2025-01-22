4b7d72ea81fc8e91bfdc593fa4d939dcef69312e
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-22 20:22:42 +0530

    Update 210. Course Schedule II.cpp

--- 210. Course Schedule II.cpp
+++ 210. Course Schedule II.cpp
@@ -46,3 +46,50 @@ public:
         return topoSortCourses(adj, numCourses, indegree);
     }
 };
+
+// Using DFS + Cycle Detection
+class Solution {
+public:
+    void topoSortCourses(unordered_map<int, vector<int>> &adj, int u, vector<bool> &visited, vector<bool> &currRec, stack<int> &st, bool &isCycle){
+        visited[u] = true;
+        currRec[u] = true;
+
+        for(int &v : adj[u]){
+            if(!visited[v])
+                topoSortCourses(adj, v, visited, currRec, st, isCycle);
+            else if(currRec[v])
+                isCycle = true;   
+        }
+        currRec[u] = false;
+        st.push(u);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> visited(numCourses, false);
+        vector<bool> currRec(numCourses, false);
+        stack<int> st;
+        unordered_map<int, vector<int>> adj;
+
+        for(auto &vec : prerequisites){
+            int u = vec[1];
+            int v = vec[0];
+            adj[u].push_back(v);
+        }
+
+        bool isCycle = false;
+        for(int i=0; i<numCourses; i++){
+            if(!visited[i])
+                topoSortCourses(adj, i, visited, currRec, st, isCycle);
+
+        }
+
+        if(isCycle) return {};
+
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        return ans;
+    }
+};
