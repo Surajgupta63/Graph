92e42fdbf91e3abbbe99848c8cea0264b49db349
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-22 22:57:04 +0530

    Update Bipartite Graph.cpp

--- Bipartite Graph.cpp
+++ Bipartite Graph.cpp
@@ -26,3 +26,39 @@ class Solotion{
         return true;
     }
 };
+
+// Using BFS
+class Solution{
+  public:
+    bool isBipartiteBFS(vector<vector<int>>& adj, int curr, vector<int> &color, int currColor){
+        queue<int> q;
+        q.push(curr);
+        color[curr] = currColor;
+        
+        while(!q.empty()){
+            int newCurr = q.front();
+            q.pop();
+            
+            for(int &v : adj[newCurr]){
+                if(color[v] == color[newCurr]) return false;
+                if(color[v] == -1){
+                    color[v] = 1 - color[newCurr];
+                    q.push(v);
+                }
+            }
+        }
+        
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> color(V, -1);
+        
+        for(int i=0; i<V; i++){
+            if(color[i] == -1 && isBipartiteBFS(adj, i, color, 1) == false){
+                return false;
+            }
+        }
+        return true;
+    }
+};
