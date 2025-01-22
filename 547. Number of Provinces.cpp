79676252e7f0cb6aa118fbfe9f7f739ab205ad0a
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-22 18:19:17 +0530

    Update 547. Number of Provinces.cpp

--- 547. Number of Provinces.cpp
+++ 547. Number of Provinces.cpp
@@ -68,3 +68,40 @@ public:
         return count;
     }
 };
+
+// Using BFS
+// T.C : O(V+E)
+// S.C : O(V)
+class Solution {
+public:
+    void BFS(vector<vector<int>>& isConnected, int u, vector<bool> &visited){
+        queue<int> q;
+        q.push(u);
+        visited[u] = true;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int v=0; v<isConnected.size(); v++){
+                if(!visited[v] && isConnected[node][v] == 1){
+                    BFS(isConnected, v, visited);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<bool> visited(V, false);
+
+        int count = 0;
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                BFS(isConnected, i, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
