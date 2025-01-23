941b58e5c9860bc6a6bc3572ab91c175cf9d7fa9
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-23 19:20:10 +0530

    Update Undirected Graph Cycle.cpp

--- Undirected Graph Cycle.cpp
+++ Undirected Graph Cycle.cpp
@@ -71,3 +71,55 @@ class Solution{
     }
 };
 
+
+// Using DSU 
+// T.C : O(V+E)
+// S.C : O(V)
+class Solution{
+    public:
+    
+    int find(int x, vector<int> &parent){
+        if(x == parent[x]) return x;
+        
+        return parent[x] = find(parent[x], parent);
+    }
+    
+    void Union(int x, int y, vector<int> &parent, vector<int> &rank){
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+        
+        if(x_parent == y_parent) return;
+        
+        if(rank[x_parent] > rank[y_parent])
+            parent[y_parent] = x_parent;
+        else if(rank[x_parent] < rank[y_parent])
+            parent[x_parent] = y_parent;
+        else{
+            parent[x_parent] = y_parent;
+            rank[y_parent] += 1;
+        }
+    }
+    
+	int detectCycle(int V, vector<int>adj[])
+	{
+	    vector<int> parent(V, -1);
+	    vector<int> rank(V, 1);
+	    
+	    for(int i=0; i<V; i++){
+	        parent[i] = i;
+	    }
+	    
+	    for(int u=0; u<V; u++){
+	        for(int &v : adj[u]){
+	            if(u < v){
+	                int u_parent = find(u, parent);
+	                int v_parent = find(v, parent);
+	                if(u_parent == v_parent) return true;
+	                Union(u_parent, v_parent, parent, rank);
+	            }
+	        }
+	    }
+	    return false;
+	}
+};
+
