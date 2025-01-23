cbadf6025e1706b69c743d11f695cd623f6be2e1
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-23 18:51:02 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -13,3 +13,28 @@ void DSUunion(int x, int y, vector<int> parent){
         parent[x_parent] = y_parent;
     }
 }
+
+// Disjoint Set Union By Rank and Path Compression
+int find(int i, vector<int> parent){
+    if(i == parent[i]) return i;
+
+    return parent[i] = find(parent[i], parent);
+}
+
+void DSUunion(int x, int y, vector<int> parent, vector<int> rank){
+    int x_parent = find(x, parent);
+    int y_parent = find(y, parent);
+
+    if(x_parent == y_parent){
+        return;
+    }
+
+    if(rank[x_parent] > rank[y_parent])
+        parent[y_parent] = x_parent;
+    else if(rank[x_parent] < rank[y_parent])
+        parent[x_parent] = y_parent;
+    else{
+        parent[x_parent] = y_parent;
+        rank[y_parent]  += 1;
+    }
+}
