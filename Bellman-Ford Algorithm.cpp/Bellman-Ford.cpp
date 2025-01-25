67c3667e709980baa68c8e276ceda8f115d27930
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-25 21:57:04 +0530

    Update Bellman-Ford.cpp

--- Bellman-Ford Algorithm.cpp/Bellman-Ford.cpp
+++ Bellman-Ford Algorithm.cpp/Bellman-Ford.cpp
@@ -9,11 +9,11 @@ class Solution {
      *   V: number of vertices
      */
     vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
-        vector<int> result(V, 1e8);
+        vector<int> result(V, 1e8); // S.C : O(V)
         result[src] = 0;
         
-        for(int i=1; i<=V-1; i++){
-            for(auto &edge : edges){
+        for(int i=1; i<=V-1; i++){ // T.C : O(V)
+            for(auto &edge : edges){ // T.C : O(E)
                 int u = edge[0];
                 int v = edge[1];
                 int w = edge[2];
