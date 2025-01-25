dc80965f65dc2501af5ac7acbc4907782313ad96
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-25 21:56:03 +0530

    Create Bellman-Ford.cpp

---
+++ Bellman-Ford Algorithm.cpp/Bellman-Ford.cpp
@@ -0,0 +1,38 @@
+// Bellman-FOrd Algorithm
+// T.C : O(V*E)
+// S.C : O(V)
+class Solution {
+  public:
+    /*  Function to implement Bellman Ford
+     *   edges: vector of vectors which represents the graph
+     *   src: source vertex
+     *   V: number of vertices
+     */
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        vector<int> result(V, 1e8);
+        result[src] = 0;
+        
+        for(int i=1; i<=V-1; i++){
+            for(auto &edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(result[u] != 1e8 && result[u] + w < result[v]){
+                    result[v] = result[u] + w;
+                }
+            }
+        }
+        
+        for(auto &edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(result[u] != 1e8 && result[u] + w < result[v]){
+                    return {-1};
+                }
+        }
+        return result;
+    }
+};
