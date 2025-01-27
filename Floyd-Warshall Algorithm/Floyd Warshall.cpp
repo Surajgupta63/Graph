9156bc460a5a20657bbeb6cd6977cd60b22c2564
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-27 16:39:34 +0530

    Create Floyd Warshall.cpp

---
+++ Floyd-Warshall Algorithm/Floyd Warshall.cpp
@@ -0,0 +1,38 @@
+// T.C : O(N^3)
+// S.C : O(1)
+class Solution {
+  public:
+    void shortestDistance(vector<vector<int>>& grid){
+        int n = grid.size();
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == -1)
+                    grid[i][j] = 100000;
+                
+            }
+        }
+        
+        for(int via = 0; via < n; via++) {
+            
+            for(int i = 0; i<n; i++) {
+                for(int j = 0; j<n; j++) {
+                    
+                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+                    
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                
+                if(grid[i][j] == 100000)
+                    grid[i][j] = -1;
+                
+            }
+        }
+	}
+};
