31e87c75d19a6b8aa3c6381ed419b3a449da8e96
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-28 19:54:05 +0530

    Create Euler circuit and Path.cpp

---
+++ GFG_Problems/Euler circuit and Path.cpp
@@ -0,0 +1,47 @@
+
+class Solution{
+  public:
+    void DFS(vector<int>adj[], int u, vector<bool> &visited){
+        visited[u] = true;
+        
+        for(auto &v : adj[u]){
+            if(!visited[v]){
+                DFS(adj, v, visited);
+            }
+        }
+    }
+    
+    bool isConnected(int V, vector<int>adj[]){
+        int nonZeroDegree = -1;
+        for(int i=0; i<V; i++){
+            if(adj[i].size() > 0){
+                nonZeroDegree = i;
+                break;
+            }
+        }
+        
+        vector<bool> visited(V, false);
+        DFS(adj, nonZeroDegree, visited);
+        
+        for(int i=0; i<V; i++){
+            if(visited[i] == false && adj[i].size() > 0) return false;
+        }
+        return true;
+    }
+
+	int isEulerCircuit(int V, vector<int>adj[]){
+	    
+	    if(isConnected(V, adj) == false) return 0;
+	    
+	    int oddDegree = 0;
+	    
+	    for(int i=0; i<V; i++){
+	        if(adj[i].size() % 2 != 0) oddDegree++;
+	    }
+	    
+	    if(oddDegree > 2) return 0;
+	    if(oddDegree == 2) return 1;
+	    
+	    return 2;
+	}
+};
